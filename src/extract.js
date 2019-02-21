c6be92ffaf6d2c3882e3031c1b404db7e616c100
austincunningham <austincunningham@hotmail.com>
2019-02-21 21:55:42 +0000

    attempt to move the files

--- src/extract.js
+++ src/extract.js
@@ -2,10 +2,15 @@
 const prompt = require('prompt');
 const colors = require('colors/safe');
 const util = require('util');
+const exec = require('sync-exec');
 const tarball = require('tarball-extract');
+const request = require('request');
 const fs = require('fs');
 const download_oc = require('./download');
 const readlineSync = require('readline-sync');
+
+const ocpath = "openshift-origin-client-tools-v";
+
 
 var extract_oc = function (callback){
     let version = readlineSync.question(colors.blue('What version of oc do you wish to install ? \n - 3.7\n - 3.9\n - 3.10\n - 3.11\n'));
@@ -16,11 +21,18 @@ var extract_oc = function (callback){
             console.log("file read error: "+err);
         } else {
             tarball.extractTarball(version+".tar.gz", version, function(error){
-                console.log("do i get here")
                 if(error) {
-                console.log("tarball error: "+error);
+                    console.log("tarball error: "+error);
                 } else {
-                console.log("File extracted");
+                    console.log("File extracted");
+                    
+                    // move the files and remove the old directory
+                    exec('../script/moveFile.sh',{cmd:version});
+                    // var mvFiles='../'+version+'/'+ocpath+version+'* ../'+version;
+                    // exec('mv',{cmd: mvFiles}, 10000);
+                    // var removedir='rm -rf ../'+version+'/'+ocpath+version+'*';
+                    // exec(removedir, 10000);
+                    
                 }
             });
         }
