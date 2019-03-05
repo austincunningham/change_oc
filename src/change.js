1e0c7d8db529e4d38650103b089f2662bd850be5
austincunningham <austincunningham@hotmail.com>
2019-03-05 22:57:29 +0000

    added eslint and modified code to suit

--- src/change.js
+++ src/change.js
@@ -1,5 +1,5 @@
 
-//'use strict';
+// 'use strict';
 const prompt = require('prompt');
 const colors = require('colors/safe');
 const util = require('util');
@@ -8,28 +8,26 @@ const readlineSync = require('readline-sync');
 const fs = require('fs');
 
 
-let child;
-
-
-var change = function(callback){
+// eslint-disable-next-line func-names
+function change(callback) {
   prompt.start();
   prompt.message = colors.green('-->');
   prompt.delimiter = colors.green(':');
 
-  console.log(colors.green("================================= Change OC ========================================"));
-  var version = readlineSync.question(colors.green('What version of oc do you wish to switch to ? \n - 3.7\n - 3.9\n - 3.10\n - 3.11\n'));
-  if (version != 3.7 && version !=  3.9 && version !=  3.10 && version !=  3.11){
-    console.log("Version not present");
-  } else {
-    if (fs.existsSync("/opt/openshift/"+version)) {
-      console.log("Command-line input received:");
-      console.log('Change to Version : ' + version);
-      
-      exec("sudo rm /usr/bin/oc && sudo ln -s /opt/openshift/"+version+"/oc /usr/bin/oc")
-      .then(changeToVersion => exec("oc version"))
-      .then(newVersion => {
-        console.log("New Version Number : ",newVersion.stdout);
-        if (newVersion.stderr){
+  console.log(colors.green('================================= Change OC ========================================'));
+  const version = readlineSync.question(colors.green('What version of oc do you wish to switch to ? \n - 3.7\n - 3.9\n - 3.10\n - 3.11\n'));
+  // eslint-disable-next-line eqeqeq
+  if (version != 3.7 && version != 3.9 && version != 3.10 && version != 3.11) {
+    console.log('Version not present');
+  } else if (fs.existsSync(`/opt/openshift/${version}`)) {
+    console.log('Command-line input received:');
+    console.log(`Change to Version : ${version}`);
+    exec(`sudo rm /usr/bin/oc && sudo ln -s /opt/openshift/${version}/oc /usr/bin/oc`)
+      .then(() => exec('oc version'))
+      // eslint-disable-next-line consistent-return
+      .then((newVersion) => {
+        console.log('New Version Number : ', newVersion.stdout);
+        if (newVersion.stderr) {
           return newVersion.stderr;
         }
 
@@ -37,13 +35,10 @@ var change = function(callback){
           callback();
         }
       })
-      .catch(Error)
-    } else {
-      console.log("Binary version "+version+" not extracted");
-      return;
-    }
+      .catch(Error);
+  } else {
+    console.log(`Binary version ${version} not extracted`);
   }
 }
 
 module.exports = change;
-
--- src/download.js
+++ src/download.js
@@ -1,15 +1,14 @@
-'use strict';
+/* eslint-disable camelcase */
+
+
 const prompt = require('prompt');
 const colors = require('colors/safe');
 const request = require('request');
 const fs = require('fs');
 const readlineSync = require('readline-sync');
-const extract_oc = require('./extract');
-const change_oc = require('./change')
 const cliAnimation = require('chalk-animation');
-
-
-let child;
+const extract_oc = require('./extract');
+const change_oc = require('./change');
 
 
 prompt.start();
@@ -20,62 +19,61 @@ prompt.delimiter = colors.green(':');
 // get the version number
 //
 
-var download_oc = function(callback){
+// eslint-disable-next-line camelcase
+function download_oc() {
   let url;
-  console.log(colors.blue("================================= Download OC ======================================"))
-  let result = readlineSync.question(colors.blue("Do you wish to install an oc binary yes/no ? "));
-  if (result == "yes" || result == "y"){
-    let version = readlineSync.question(colors.blue('What version of oc do you wish to install ? \n - 3.7\n - 3.9\n - 3.10\n - 3.11\n'));
+  console.log(colors.blue('================================= Download OC ======================================='));
+  const result = readlineSync.question(colors.blue('Do you wish to install an oc binary yes/no ? '));
+  if (result === 'yes' || result === 'y') {
+    const version = readlineSync.question(colors.blue('What version of oc do you wish to install ? \n - 3.7\n - 3.9\n - 3.10\n - 3.11\n'));
     exports.version = version;
-    switch(version) {
-      case "3.7":
-          url = "https://github.com/openshift/origin/releases/download/v3.7.2/openshift-origin-client-tools-v3.7.2-282e43f-linux-64bit.tar.gz"
-          break;
-      case "3.9":
-          url = 'https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz';
-          break;
-      case "3.10":
-          url = "https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz"
-          break;
-      case "3.11":
-          url = "https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz"
-          break;
+    switch (version) {
+      case '3.7':
+        url = 'https://github.com/openshift/origin/releases/download/v3.7.2/openshift-origin-client-tools-v3.7.2-282e43f-linux-64bit.tar.gz';
+        break;
+      case '3.9':
+        url = 'https://github.com/openshift/origin/releases/download/v3.9.0/openshift-origin-client-tools-v3.9.0-191fece-linux-64bit.tar.gz';
+        break;
+      case '3.10':
+        url = 'https://github.com/openshift/origin/releases/download/v3.10.0/openshift-origin-client-tools-v3.10.0-dd10d17-linux-64bit.tar.gz';
+        break;
+      case '3.11':
+        url = 'https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz';
+        break;
       default:
-          url = false;
-          console.log(colors.blue("No binary present"));
-          break;
+        url = false;
+        console.log(colors.blue('No binary present'));
+        break;
     }
 
-    if(url && !(fs.existsSync(version+".tar.gz")) ) {
-      var req = request({
-          uri: url,
-          method: "GET",
-          timeout: 10000,
-          followRedirect: true,
-          maxRedirects: 10
-        }).pipe(fs.createWriteStream(version +".tar.gz"))
-        // Add loading animation
-        const loading = cliAnimation.radar('===================================================================================>'); 
-      req.on('close', function(){
+    if (url && !(fs.existsSync(`${version}.tar.gz`))) {
+      const req = request({
+        uri: url,
+        method: 'GET',
+        timeout: 10000,
+        followRedirect: true,
+        maxRedirects: 10,
+      }).pipe(fs.createWriteStream(`${version}.tar.gz`));
+      // Add loading animation
+      cliAnimation.radar('===================================================================================>');
+      req.on('close', () => {
         console.log(colors.blue('Request finished writing to file'));
-        extract_oc(function(){
+        extract_oc(() => {
           console.log('Extract completed');
         });
-      });  
-        
+      });
     } else {
       console.log(colors.blue('Binary Already Download'));
-      extract_oc(function(){
+      extract_oc(() => {
         console.log('Extract completed');
       });
-    }  
-  } else{ 
+    }
+  } else {
     console.log(colors.blue('Skipping Binary Download'));
-    change_oc(function(){
+    change_oc(() => {
       console.log('Extract completed');
     });
   }
-};
+}
 
 module.exports = download_oc;
-
