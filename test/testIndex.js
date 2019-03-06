b2025dd222a6a6f455294fde9ee1dff8ef92d6e8
austincunningham <austincunningham@hotmail.com>
2019-03-06 20:36:51 +0000

    begin adding tests

--- test/testIndex.js
+++ test/testIndex.js
@@ -1,8 +1,26 @@
 
 const { assert } = require('chai');
+const mock = require('mock-fs');
 const fixtures = require('./fixtures.json');
+const
+
 
 const { versions } = fixtures;
+
+
+// eslint-disable-next-line no-undef
+beforeEach(() => {
+  mock({
+    '/opt/': {
+      'empty-dir': {/** empty directory */},
+    },
+  });
+});
+
+// eslint-disable-next-line no-undef
+afterEach(() => {
+  mock.restore();
+});
 
 // eslint-disable-next-line no-undef
 suite('Index tests suit', () => {
