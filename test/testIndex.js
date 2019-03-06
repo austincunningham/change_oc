ea3589d557dc3f2db16c074043226ce02d91e61c
austincunningham <austincunningham@hotmail.com>
2019-03-06 22:56:18 +0000

    fix build

--- test/testIndex.js
+++ test/testIndex.js
@@ -1,27 +1,28 @@
 
 const { assert } = require('chai');
 const mock = require('mock-fs');
+const { beforeEach, afterEach } = require('mocha');
 const fixtures = require('./fixtures.json');
+
 
 const { versions } = fixtures;
 
-
-// eslint-disable-next-line no-undef
-beforeEach(() => {
-  mock({
-    '/opt/': {
-      'empty-dir': {/** empty directory */},
-    },
-  });
-});
-
-// eslint-disable-next-line no-undef
-afterEach(() => {
-  mock.restore();
-});
-
 // eslint-disable-next-line no-undef
 suite('Index tests suit', () => {
+  // eslint-disable-next-line no-undef
+  beforeEach(() => {
+    mock({
+      '/opt/': {
+        'empty-dir': {/** empty directory */},
+      },
+    });
+  });
+
+  // eslint-disable-next-line no-undef
+  afterEach(() => {
+    mock.restore();
+  });
+
   // eslint-disable-next-line no-undef
   test('first test', () => {
     console.log(versions);
