ce93423f5f46f782c6debeb9dc2aeec87dd2e9fe
Lydia Cunningham <irishlydia@gmail.com>
2020-10-08 23:38:26 +0100

    update download menu

--- src/download.js
+++ src/download.js
@@ -17,7 +17,7 @@ function download_oc() {
   console.log(colors.blue('================================= Download OC ======================================='));
   const result = readlineSync.question(colors.blue('Do you wish to install an oc binary yes/no ? '));
   if (result === 'yes' || result === 'y') {
-    const version = readlineSync.question(colors.blue('What version of oc do you wish to install ? \n - 3.7\n - 3.9\n - 3.10\n - 3.11\n - 4.1\n - 4.2\n - 4.3\n'));
+    const version = readlineSync.question(colors.blue('What version of oc do you wish to install ? \n - 3.7\n - 3.9\n - 3.10\n - 3.11\n - 4.1\n - 4.2\n - 4.3\n - 4.4\n'));
     exports.version = version;
     switch (version) {
       case '3.7':
